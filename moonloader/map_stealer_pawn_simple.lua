3f555dc955649ae5a0dedf7ab0160b21c96e9a0d
Aleks4k <84202397+Aleks4k@users.noreply.github.com>
2023-02-03 15:21:00 +0100

    Update map_stealer_pawn_simple.lua

--- moonloader/map_stealer_pawn_simple.lua
+++ moonloader/map_stealer_pawn_simple.lua
@@ -1,4 +1,4 @@
-#All logs will go to console, to turn on console click‚ button left from 1
+--All logs will go to console, to turn on console click‚ button left from 1
 local sampev = require 'lib.samp.events'
 local ev = require("samp.events")
 local bitex = require('lib.bitex')
@@ -10,13 +10,13 @@ function sampev.onRemoveBuilding(modelId, position, radius)
 end
 
 function sampev.onCreateObject(objectId, data)
-	--print(string.format("obj = CreateObject(%d, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f);", data.modelId, data.position.x, data.position.y, data.position.z, data.rotation.x, data.rotation.y, data.rotation.z))
+	print(string.format("obj = CreateObject(%d, %.6f, %.6f, %.6f, %.6f, %.6f, %.6f);", data.modelId, data.position.x, data.position.y, data.position.z, data.rotation.x, data.rotation.y, data.rotation.z))
 end
 
 function sampev.onSetObjectMaterial(objectId, material)
-	--print(string.format("SetObjectMaterial(obj, %s, %s, %s%s%s, %s%s%s, %s);", material.materialId, material.modelId, navodnik, material.libraryName, navodnik, navodnik, material.textureName, navodnik, srediRGBAobject(material.color)))
+	print(string.format("SetObjectMaterial(obj, %s, %s, %s%s%s, %s%s%s, %s);", material.materialId, material.modelId, navodnik, material.libraryName, navodnik, navodnik, material.textureName, navodnik, srediRGBAobject(material.color)))
 end
 
 function sampev.onSetObjectMaterialText(objectId, material)
-	--print(string.format("SetObjectMaterialText(obj, %s%s%s, %d, %d, %s%s%s, %d, %d, %s, %s, %d);", navodnik, material.text, navodnik, material.materialId,material.materialSize, navodnik, material.fontName, navodnik, material.fontSize,material.bold,srediRGBAobject(material.fontColor),srediRGBAobject(material.backGroundColor),material.align))
+	print(string.format("SetObjectMaterialText(obj, %s%s%s, %d, %d, %s%s%s, %d, %d, %s, %s, %d);", navodnik, material.text, navodnik, material.materialId,material.materialSize, navodnik, material.fontName, navodnik, material.fontSize,material.bold,srediRGBAobject(material.fontColor),srediRGBAobject(material.backGroundColor),material.align))
 end
